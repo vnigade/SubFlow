9c21b9dba0d9c9bc5c13d2478237ab4fdfd73c68
Pablo Bauszat <p.bauszat@tudelft.nl>
2022-04-06 15:03:31 +0200

    The SubFlow evaluation script now searches for all configurations in the sub-folders and displays them sorted by training strategy and number of epochs.

--- experiments/eval_subflow.py
+++ experiments/eval_subflow.py
@@ -3,8 +3,13 @@ Evaluates the SubFlow model.
 """
 import argparse
 import os
+import pathlib
 
-from network import SubFlow, SubFlowLeNetConfiguration
+# Disable tensorflow warnings
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+from collections import defaultdict
+from network import BaseConfiguration, SubFlow, SubFlowLeNetConfiguration
 from network.mnist import load_data, display_examples
 
 
@@ -23,28 +28,45 @@ def main():
     print(f"Test data: {x_test.shape} {y_test.shape}")
     print()
 
-    # Create configurations to evaluate
-    subflow_from_scratch_base_folder = os.path.join(args.model_base_directory, "SubFlow/FromScratch/")
-    subflow_from_lenet_base_folder = os.path.join(args.model_base_directory, "SubFlow/FromLeNet/")
-    configurations = list()
-    for utilization in range(10, 100, 10):
-        configurations.append(SubFlowLeNetConfiguration(subflow_from_scratch_base_folder, args.epochs, leaky_relu=True, utilization=utilization, seed=args.seed))
-        configurations.append(SubFlowLeNetConfiguration(subflow_from_lenet_base_folder, args.epochs, leaky_relu=True, utilization=utilization, seed=args.seed))
+    # Specifiy which training strategies to evaluate
+    training_strategies = ["FromScratch", "FromLeNet"]
 
-    # Evaluate configurations
-    for configuration in configurations:
-        # Instantiate and load the model
-        network = SubFlow(None, configuration.leaky_relu, configuration.utilization, configuration.seed)
-        model_directory = os.path.join(configuration.model_base_directory, configuration.path)
-        network.load(model_directory)
+    # Find all configurations for the training strategies
+    subflow_base_folder = os.path.join(args.model_base_directory, "SubFlow")
+    strategy_configurations = defaultdict(list)
+    for strategy in training_strategies:
+        strategy_folder = os.path.join(subflow_base_folder, strategy)
+        for config_file in pathlib.Path(strategy_folder).rglob("train.configuration"):
+            configuration = BaseConfiguration.load_from_file(str(config_file))
+            assert isinstance(configuration, SubFlowLeNetConfiguration)
+            # Group configurations by training strategy and number of training epochs
+            strategy_configurations[(strategy, configuration.epochs)].append(configuration)
 
-        # Evaluate the network
-        metrics = network.evaluate(x_test, y_test)
-        print(f"{network.name} (folder={configuration.model_base_directory}, epochs={configuration.epochs}, leaky_relu={configuration.leaky_relu}, utilization={configuration.utilization}): {metrics}")
+    # Evaluate strategies
+    for (strategy, epochs), configurations in strategy_configurations.items():
+        # Display strategy header
+        print("*" * 100)
+        print(f"Training strategy: {strategy} - Epochs: {epochs}")
+        print("*" * 100)
 
-        # Display some examples
-        if args.display_examples:
-            display_examples(network, test)
+        # Evaluate configurations for strategy
+        for configuration in configurations:
+            # Instantiate and load the model
+            network = SubFlow(None, configuration.leaky_relu, configuration.utilization, configuration.seed)
+            model_directory = os.path.join(configuration.model_base_directory, configuration.path)
+            network.load(model_directory)
+
+            # Evaluate the network
+            metrics = network.evaluate(x_test, y_test)
+
+            # Print metrics
+            print(f"utilization={configuration.utilization}, leaky_relu={configuration.leaky_relu}, initalization_directory={configuration.initialization_directory}: {metrics}")
+
+            # Display some examples
+            if args.display_examples:
+                display_examples(network, test)
+
+        print()
 
 
 if __name__ == "__main__":

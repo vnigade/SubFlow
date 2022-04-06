1e3128590e9886b352a54b4a0b61ec6b9f5eb10f
Pablo Bauszat <p.bauszat@tudelft.nl>
2022-04-06 17:01:43 +0200

    Updated collection per-processing and weight visualization.

--- experiments/preprocess_into_collection.py
+++ experiments/preprocess_into_collection.py
@@ -1,8 +1,5 @@
 """
 Collects the Network data extracted by the extract_from_subflow.py script and stores it into a NetworkCollection.
-
-todo: currently this is hard-coded for MNIST dataset and 10 utilizations.
-
 """
 import argparse
 import os
@@ -19,7 +16,7 @@ from network import BaseConfiguration, SubFlow, SubFlowLeNetConfiguration
 
 def load_from_original_subflow(base_data_folder: str) -> NetworkCollection:
     # Define input folders and utilization
-    # todo: this should be passed in as arguments.
+    # todo: currently this is hard-coded for MNIST dataset and 10 utilizations and should be passed in as arguments.
     inputs = {1.0: "network1",
               0.9: "sub_network9",
               0.8: "sub_network8",
@@ -36,21 +33,26 @@ def load_from_original_subflow(base_data_folder: str) -> NetworkCollection:
     return NetworkCollection(networks)
 
 
-def load_from_models(base_data_folder: str) -> NetworkCollection:
-    networks = list()
-    for config_file in pathlib.Path(base_data_folder).rglob("train.configuration"):
+def load_from_models(base_model_folder: str, epochs: int) -> NetworkCollection:
+    # Find and load configuration files in the folder that match the epochs
+    configurations = dict()
+    for config_file in pathlib.Path(base_model_folder).rglob("train.configuration"):
         # Load configuration file
         configuration = BaseConfiguration.load_from_file(str(config_file))
         assert isinstance(configuration, SubFlowLeNetConfiguration)
-        print(configuration)
+        if configuration.epochs == epochs:
+            model_directory = str(config_file.parent)
+            configurations[configuration.utilization] = (model_directory, configuration)
 
+    # Load the networks in descending order of utilization
+    networks = list()
+    for utilization, (model_directory, configuration) in reversed(sorted(configurations.items())):
         # Load network
-        model_directory = str(config_file.parent)
-        network = SubFlow(configuration.initialization_directory, configuration.leaky_relu, configuration.utilization, configuration.seed)
+        network = SubFlow(None, configuration.leaky_relu, configuration.utilization, configuration.seed)
         network.load(model_directory)
 
         # Convert the data from the network to a NetworkData structure
-        # todo: get better acces to the network layers
+        # todo: get better access to the network layers
         layers = list()
         for i, layer in enumerate(network._model.layers):
             if isinstance(layer, (tf.keras.layers.Dense, tf.keras.layers.Conv2D)):
@@ -71,15 +73,17 @@ def load_from_models(base_data_folder: str) -> NetworkCollection:
 def main():
     # Parse arguments
     args = argparse.ArgumentParser()
-    args.add_argument("--base_data_folder", type=str, default="./models/SubFlow/FromLeNet", help="The original base data folder.")
+    args.add_argument("--base_data_folder", type=str, default="./models/SubFlow/Progressive", help="The original base data folder.")
     args.add_argument("--original_base_data_folder", type=str, default="../output/", help="The original base data folder.")
+    args.add_argument("--epochs", type=int, default=5, help="The number of training epochs.")
     args.add_argument("--output_folder", type=str, default="./data", help="The output folder.")
     args.add_argument("--output_filename", type=str, default="collection.pickle", help="The output file name.")
     args = args.parse_args()
 
     # Load networks into collection
+    # todo: currently all architectures are packed into one collection which is not desired.
     # collection = load_from_original_subflow(args.original_base_data_folder)
-    collection = load_from_models(args.base_data_folder)
+    collection = load_from_models(args.base_data_folder, args.epochs)
     print(collection)
 
     # Save collection
--- experiments/visualize_weight_bias.py
+++ experiments/visualize_weight_bias.py
@@ -4,6 +4,7 @@ Visualizes weights and biases that were extracted using the extract_data.py scri
 import argparse
 import numpy as np
 import os
+import pathlib
 
 from analysis import NetworkCollection, Visualizer
 
@@ -21,6 +22,28 @@ def quantile(array: np.ndarray, lower: float = 0.05, upper: float = 0.95) -> tup
     return np.quantile(array, lower), np.quantile(array, upper)
 
 
+def plot_networks(output_path: str, collection: NetworkCollection) -> None:
+    min_weight_value, max_weight_value = collection.weight_minmax
+    min_bias_value, max_bias_value = collection.bias_minmax
+    for network in collection.networks:
+        output_file = os.path.join(output_path, f"network_{network.utilization:.2f}_image.png")
+        Visualizer.plot_network(network, output_file, min_weight_value, max_weight_value, min_bias_value, max_bias_value, show_figure=False)
+
+
+def plot_network_differences(output_path: str, differences: tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]) -> None:
+    difference_to_base, difference_to_previous, difference_ratio_to_base, difference_ratio_to_previous = differences
+    Visualizer.plot_arrays(array_columns_to_list(difference_to_base), os.path.join(output_path, "difference_to_base.png"), data_range=None)
+    Visualizer.plot_arrays(array_columns_to_list(difference_to_previous), os.path.join(output_path, "difference_to_previous.png"), data_range=None)
+    Visualizer.plot_arrays(array_columns_to_list(difference_ratio_to_base), os.path.join(output_path, "difference_ratio_to_base.png"), data_range=None)
+    Visualizer.plot_arrays(array_columns_to_list(difference_ratio_to_previous), os.path.join(output_path, "difference_ratio_to_previous.png"), data_range=None)
+
+
+def plot_weight_histograms(output_path: str, combined_weights: np.ndarray, difference_to_base: np.ndarray, difference_to_previous: np.ndarray) -> None:
+    Visualizer.plot_histogram(combined_weights, os.path.join(output_path, "histogram_combined_weights.png"), data_range=None)
+    Visualizer.plot_histogram(difference_to_base, os.path.join(output_path, "histogram_difference_to_base.png"), data_range=None)
+    Visualizer.plot_histogram(difference_to_previous, os.path.join(output_path, "histogram_difference_to_previous.png"), data_range=None)
+
+
 # =================================================================================================
 # Main
 # =================================================================================================
@@ -28,42 +51,31 @@ def quantile(array: np.ndarray, lower: float = 0.05, upper: float = 0.95) -> tup
 def main():
     # Parse arguments
     args = argparse.ArgumentParser()
-    args.add_argument("--collection_file", type=str, default="./data/from_lenet_collection.pickle", help="The network collection to load.")
-    args.add_argument("--plot_folder", type=str, default="./data", help="The folder for outputting the plots.")
+    args.add_argument("--collection_file", type=str, default="./data/from_scratch_collection.pickle", help="The network collection to load.")
+    args.add_argument("--plot_base_folder", type=str, default="./data", help="The folder for outputting the plots.")
     args = args.parse_args()
 
     # Load network collection
     collection = NetworkCollection.load_from_file(args.collection_file)
     print(collection)
 
-    # Plot networks
-    min_weight_value, max_weight_value = collection.weight_minmax
-    min_bias_value, max_bias_value = collection.bias_minmax
-    for network in collection.networks:
-        output_file = os.path.join(args.plot_folder, f"visualization_{network.utilization:.2f}.png")
-        Visualizer.plot_network(network, output_file, min_weight_value, max_weight_value, min_bias_value, max_bias_value, show_figure=False)
-
-    # Compute differences and data quantiles
+    # Compute weight differences
     combined_weights = collection.combined_weights
     base_weights = combined_weights[:, 0].reshape(-1, 1)
-    weight_base_differences = combined_weights[:, 1:] - base_weights
-    weight_previous_differences = np.diff(combined_weights, axis=1)
-    weight_base_differences_ratio = np.abs(weight_base_differences) / np.abs(base_weights)
-    weight_previous_differences_ratio = np.abs(weight_previous_differences) / np.abs(base_weights)
+    difference_to_base = combined_weights[:, 1:] - base_weights
+    difference_to_previous = np.diff(combined_weights, axis=1)
+    difference_ratio_to_base = np.abs(difference_to_base) / np.abs(base_weights)
+    difference_ratio_to_previous = np.abs(difference_to_previous) / np.abs(combined_weights[:, 0:-1])
 
-    # Plot weights data and histograms
-    Visualizer.plot_arrays(array_columns_to_list(weight_base_differences), os.path.join(args.plot_folder, "weight_base_differences.png"), data_range=quantile(base_weights), show_figure=False)
-    Visualizer.plot_arrays(array_columns_to_list(weight_previous_differences), os.path.join(args.plot_folder, "weight_previous_differences.png"), data_range=quantile(weight_previous_differences),
-                           show_figure=False)
-    Visualizer.plot_arrays(array_columns_to_list(weight_base_differences_ratio), os.path.join(args.plot_folder, "weight_base_differences_ratio.png"),
-                           data_range=quantile(weight_base_differences_ratio), show_figure=False)
-    Visualizer.plot_arrays(array_columns_to_list(weight_previous_differences_ratio), os.path.join(args.plot_folder, "weight_previous_differences_ratio.png"),
-                           data_range=quantile(weight_previous_differences_ratio),
-                           show_figure=False)
+    # Create plot output folder
+    collection_path = pathlib.Path(args.collection_file)
+    plot_folder = os.path.join(args.plot_base_folder, collection_path.stem)
+    pathlib.Path(plot_folder).mkdir(parents=True, exist_ok=True)
 
-    Visualizer.plot_histogram(combined_weights, os.path.join(args.plot_folder, "combined_weights_histogram.png"), data_range=quantile(combined_weights))
-    Visualizer.plot_histogram(weight_base_differences, os.path.join(args.plot_folder, "weight_base_differences_histogram.png"), data_range=quantile(weight_base_differences))
-    Visualizer.plot_histogram(weight_previous_differences, os.path.join(args.plot_folder, "weight_previous_differences_histogram.png"), data_range=quantile(weight_previous_differences))
+    # Plot networks, weight histograms, and network differences
+    plot_networks(plot_folder, collection)
+    plot_network_differences(plot_folder, (difference_to_base, difference_to_previous, difference_ratio_to_base, difference_ratio_to_previous))
+    plot_weight_histograms(plot_folder, combined_weights, difference_to_base, difference_to_previous)
 
 
 if __name__ == "__main__":

787226f264b340bebc4a69392bbcba126b60c711
Pablo Bauszat <p.bauszat@tudelft.nl>
2022-04-07 17:08:16 +0200

    Improved compression experiment evaluation.

--- experiments/compression_experiment.py
+++ experiments/compression_experiment.py
@@ -12,6 +12,7 @@ import sys
 import tensorflow as tf
 
 from analysis import NetworkCollection, NetworkData, NetworkDifference, NetworkDifferenceCollection
+from dataclasses import dataclass
 from network import SubFlow
 from network.mnist import load_data
 
@@ -94,6 +95,36 @@ def compress_differences(differences: NetworkDifferenceCollection, compression_r
 # Evaluation
 # =================================================================================================
 
+@dataclass
+class DifferenceStatistic:
+    weight_differences_count: int
+    nonzero_weight_differences_count: int
+    bias_differences_count: int
+    nonzero_bias_differences_count: int
+    byte_size: int
+    nonzero_byte_size: int
+
+    def __init__(self, differences: NetworkDifferenceCollection):
+        self.weight_differences_count = differences.weight_differences_count
+        self.nonzero_weight_differences_count = differences.nonzero_weight_differences_count
+        self.bias_differences_count = differences.bias_differences_count
+        self.nonzero_bias_differences_count = differences.nonzero_bias_differences_count
+        self.byte_size = differences.byte_size
+        self.nonzero_byte_size = differences.nonzero_byte_size
+
+    @property
+    def nonzero_weight_differences_percentage(self) -> float:
+        return float(self.nonzero_weight_differences_count) / float(self.weight_differences_count)
+
+    @property
+    def nonzero_bias_differences_percentage(self) -> float:
+        return float(self.nonzero_bias_differences_count) / float(self.bias_differences_count)
+
+    @property
+    def nonzero_byte_size_percentage(self) -> float:
+        return float(self.nonzero_byte_size) / float(self.byte_size)
+
+
 def to_megabytes(x: int) -> float:
     return float(x) / (1024.0 ** 2)
 
@@ -118,11 +149,11 @@ def display_collection_statistics(collection: NetworkCollection) -> None:
     logging.info(f"Non-zero byte size: {megabytes_and_percentage(collection.nonzero_byte_size, collection.byte_size)}")
 
 
-def display_differences_statistics(differences: NetworkDifferenceCollection) -> None:
-    logging.info(f"Number of non-zero weight differences: {value_and_percentage(differences.nonzero_weight_differences_count, differences.weight_differences_count)}")
-    logging.info(f"Number of non-zero bias differences: {value_and_percentage(differences.nonzero_bias_differences_count, differences.bias_differences_count)}")
-    logging.info(f"Differences byte size: {to_megabytes(differences.byte_size):.1f} MB")
-    logging.info(f"Non-zero differences byte size: {megabytes_and_percentage(differences.nonzero_byte_size, differences.byte_size)}")
+def display_differences_statistics(statistic: DifferenceStatistic) -> None:
+    logging.info(f"Number of non-zero weight differences: {statistic.nonzero_weight_differences_count} ({statistic.nonzero_weight_differences_percentage * 100.0:.1f}%)")
+    logging.info(f"Number of non-zero bias differences: {statistic.nonzero_bias_differences_count} ({statistic.nonzero_bias_differences_percentage * 100.0:.1f}%)")
+    logging.info(f"Differences byte size: {to_megabytes(statistic.byte_size):.1f} MB")
+    logging.info(f"Non-zero differences byte size: {statistic.nonzero_byte_size} ({statistic.nonzero_byte_size_percentage * 100.0:.1f}%)")
 
 
 def eval_base_differences(collection: NetworkCollection, differences: NetworkDifferenceCollection, x: np.ndarray, y: np.ndarray) -> pd.Series:
@@ -186,13 +217,18 @@ def main():
     args = argparse.ArgumentParser()
     args.add_argument("--collection_file", type=str, default="./data/progressive_leaky_5epochs.collection", help="The network collection to load.")
     args.add_argument("--output_directory", type=str, default="./results", help="The output log file.")
-    args.add_argument("--relative_to_base", type=bool, default=False, action=argparse.BooleanOptionalAction, help="Toggles the computation of the differences relative to the previous or base.")
-    args.add_argument("--individual_compression", type=bool, default=False, action=argparse.BooleanOptionalAction, help="Toggles overall or individual network compression.")
+    args.add_argument("--relative_to_base", type=bool, default=True, action=argparse.BooleanOptionalAction, help="Toggles the computation of the differences relative to the previous or base.")
+    args.add_argument("--individual_compression", type=bool, default=True, action=argparse.BooleanOptionalAction, help="Toggles overall or individual network compression.")
     args = args.parse_args()
 
-    # Setup file logging
+    # Define a base name for the experiment
     collection_file = pathlib.Path(args.collection_file)
-    log_file = os.path.join(args.output_directory, f"{collection_file.stem}_compression.log")
+    relative_to_base_str = "base" if args.relative_to_base else "previous"
+    individual_compression_str = "individual" if args.individual_compression else "overall"
+    base_name = f"{collection_file.stem}_{relative_to_base_str}_{individual_compression_str}"
+
+    # Setup file logging
+    log_file = os.path.join(args.output_directory, f"{base_name}_compression.log")
     logging.basicConfig(filename=log_file, filemode="w", level=logging.DEBUG, format="%(message)s")
     logger = logging.getLogger()
     logger.addHandler(logging.StreamHandler(sys.stdout))
@@ -217,7 +253,8 @@ def main():
         differences = NetworkDifferenceCollection.previous_differences_from_collection(collection)
 
     logging.info("Original differences:")
-    display_differences_statistics(differences)
+    difference_statistic = DifferenceStatistic(differences)
+    display_differences_statistics(difference_statistic)
     logging.info("")
 
     # Evaluate SubFlow with the original weights
@@ -230,22 +267,44 @@ def main():
 
     # Evaluate SubFlow with various compression rates
     all_accuracies = list()
+    all_statistics = list()
     for compression_rate in range(10, 100, 10):
         compressed_differences = compress_differences(differences, compression_rate, args.individual_compression)
+        compressed_difference_statistic = DifferenceStatistic(compressed_differences)
+        all_statistics.append(compressed_difference_statistic)
 
         logging.info("=" * 150)
         logging.info(f"Compressed weights (rate = {compression_rate})")
         logging.info("-" * 150)
-        display_differences_statistics(compressed_differences)
+        display_differences_statistics(compressed_difference_statistic)
+
+        # Track the accuracies for all utilization
         accuracies = eval_differences(collection, compressed_differences, x_test, y_test, args.relative_to_base)
         all_accuracies.append(accuracies)
+
         logging.info("-" * 150)
         logging.info("")
 
-    # Write out evaluated accuracies
-    result_file = os.path.join(args.output_directory, f"{collection_file.stem}_compression.xlsx")
-    data_frame = pd.DataFrame(all_accuracies)
-    data_frame.to_excel(result_file, index=False)
+    # Write out difference statistics
+    statistics_dict = {
+        "weight_differences_count": pd.Series([s.weight_differences_count for s in all_statistics]),
+        "nonzero_weight_differences_count": pd.Series([s.nonzero_weight_differences_count for s in all_statistics]),
+        "bias_differences_count": pd.Series([s.bias_differences_count for s in all_statistics]),
+        "nonzero_bias_differences_count": pd.Series([s.nonzero_bias_differences_count for s in all_statistics]),
+        "byte_size": pd.Series([s.byte_size for s in all_statistics]),
+        "nonzero_byte_size": pd.Series([s.nonzero_byte_size for s in all_statistics]),
+        "nonzero_weight_differences_percentage": pd.Series([s.nonzero_weight_differences_percentage for s in all_statistics]),
+        "nonzero_bias_differences_percentage": pd.Series([s.nonzero_bias_differences_percentage for s in all_statistics]),
+        "nonzero_byte_size_percentage": pd.Series([s.nonzero_byte_size_percentage for s in all_statistics]),
+    }
+    statistics_file = os.path.join(args.output_directory, f"{base_name}_compression_statistics.xlsx")
+    statistics_data_frame = pd.DataFrame(statistics_dict)
+    statistics_data_frame.to_excel(statistics_file, index=False)
+
+    # Write out evaluated accuracies over the various utilization
+    accuracy_file = os.path.join(args.output_directory, f"{base_name}_compression_accuracy.xlsx")
+    accuracy_data_frame = pd.DataFrame(all_accuracies).transpose()
+    accuracy_data_frame.to_excel(accuracy_file, index=False)
 
 
 if __name__ == "__main__":
